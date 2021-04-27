b6c009779c9a78f079fa85805048683856bedef5
Levente15 <79134762+Levente15@users.noreply.github.com>
2021-04-27 18:49:26 +0300

    Test#1

--- PriorityHour/cmake-build-debug/CMakeFiles/PriorityHour.dir/DependInfo.cmake
+++ PriorityHour/cmake-build-debug/CMakeFiles/PriorityHour.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "E:/Egyetem/I ev/II felev/Adatszerkezetek_es_Algoritmusok/DSA_group_project_2/PriorityHour/main.c" "E:/Egyetem/I ev/II felev/Adatszerkezetek_es_Algoritmusok/DSA_group_project_2/PriorityHour/cmake-build-debug/CMakeFiles/PriorityHour.dir/main.c.obj"
+  "C:/Users/Legion/Desktop/Informatika/Masodik_felev/Adatszerkezetek/DSA_group_project_2/PriorityHour/main.c" "C:/Users/Legion/Desktop/Informatika/Masodik_felev/Adatszerkezetek/DSA_group_project_2/PriorityHour/cmake-build-debug/CMakeFiles/PriorityHour.dir/main.c.obj"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
--- PriorityHour/main.c
+++ PriorityHour/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Team Leade Csillag Alex!\n");
     return 0;
 }
